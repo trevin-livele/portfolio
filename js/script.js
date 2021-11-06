254580e10395f8bd8452a8625ef7e0176c26234b
trevin-livele <liveletrevin6@gmail.com>
2021-11-06 19:47:08 +0300

    separated js scripts

--- js/script.js
+++ js/script.js
@@ -1,24 +1,3 @@
-const panels = document.querySelectorAll('.panel')
-
-panels.forEach((panel) => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
-
-
-function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
-}
-
-
-
-
-
-
 const boxes = document.querySelectorAll('.box') 
 
 window.addEventListener('scroll', checkBoxes)
